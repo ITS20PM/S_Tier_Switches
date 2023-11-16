30e284d4da37ea06964a676f36165881e5909d56
ITS20PM <patrick21meng@gmail.com>
2023-11-15 16:42:28 -0800

    first commit

---
+++ src/pages/Nav.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav>Nav
+      <div>
+        <ul>
+          <li>
+            <a href="/shop">Shop</a>
+          </li>
+          
+          <li>
+            <a href="/customize">Customize</a>
+          </li>
+          
+          <li>
+            <a href="/reviews">Reviews</a>
+          </li>
+
+          <li>
+            <a href="/about">About</a>
+          </li>
+        </ul>
+      </div>
+      
+    </nav>
+  )
+}
+
+export default Nav
